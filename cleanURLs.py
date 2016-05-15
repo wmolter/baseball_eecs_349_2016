7f6dd640ba2ce856fc9fcc7bf430c5334f70d359
austinskim <austinskim@gmail.com>
2016-05-15 18:41:41 -0500

    cleanURL.py updated

--- cleanURLs.py
+++ cleanURLs.py
@@ -3,6 +3,7 @@ import requests
 urls = open('urls.txt')
 workingUrls = open('cleanURLs.txt', 'w')
 count = 1
+index = 0
 
 for url in urls:
 	if count % 10 == 0:
@@ -10,7 +11,12 @@ for url in urls:
 	url = url.strip()
 	r = requests.get(url)
 	if url == r.url:
+		tempFileName = 'HTML File_' + str(index) + '.html'
 		workingUrls.write(url+'\n')
+		htmlFile = open(tempFileName, 'w')
+		htmlFile.write(r.content)
+		htmlFile.close()
+		index += 1
 		#print 'Found one!'
 	count += 1
 
